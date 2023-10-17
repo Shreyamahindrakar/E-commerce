8dc5fdee9f096a497d0c5f58f5dae1e2d61db826
Shreya <smsonu164@gmail.com>
2023-10-17 09:28:44 +0530

    added postgresql db

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lp+_$vb775dwxep444dk_5ylu!5x(#$fv4^y48$^rqz#wsn7hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -80,13 +80,22 @@ WSGI_APPLICATION = 'shoppinglyx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':"ecommerce_db",
+        'USER': "postgres",
+        'PASSWORD': "1234",
+        'HOST': "127.0.0.1",
+        'PORT': "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
